337cc35bcf7f850e77cf72aad4ef392212a62577
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-16 02:30:19 +0300

    A Table Full of Restaurants (Services/RestaurantData)

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -4,14 +4,22 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers
 {
     public class HomeController : Controller
     {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            _restaurantData = restaurantData;
+        }
+
         public IActionResult Index()
         {
-            var model = new Restaurant {Id = 1, Name = "The House of Kobe"};
+            var model = _restaurantData.GetAll();
 
             return View(model);
         }
