2321662b238cbd5f6c1c2cb7fab3c8a67750f81d
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-18 16:24:35 +0300

    User Registration

---
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.ViewModels;
+
+namespace OdeToFood.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Register(RegisterUserViewModel model)
+        {
+            
+        }
+    }
+}
