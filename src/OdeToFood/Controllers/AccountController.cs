bd85a7413e842be4e78dce7d79865de20b08a230
Alexander Gorban <gorban.alexander.y@gmail.com>
2016-10-18 16:50:17 +0300

    Creating a User

--- src/OdeToFood/Controllers/AccountController.cs
+++ src/OdeToFood/Controllers/AccountController.cs
@@ -2,13 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Entities;
 using OdeToFood.ViewModels;
 
 namespace OdeToFood.Controllers
 {
     public class AccountController : Controller
     {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
         [HttpGet]
         public IActionResult Register()
         {
@@ -16,9 +27,26 @@ namespace OdeToFood.Controllers
         }
 
         [HttpPost, ValidateAntiForgeryToken]
-        public IActionResult Register(RegisterUserViewModel model)
+        public async Task<IActionResult> Register(RegisterUserViewModel model)
         {
-            
+            if (ModelState.IsValid)
+            {
+                var user = new User { UserName = model.Username };
+                var createResult = await _userManager.CreateAsync(user, model.Password);
+                if (createResult.Succeeded)
+                {
+                    await _signInManager.SignInAsync(user, false);
+                    return RedirectToAction("Index", "Home");
+                }
+                else
+                {
+                    foreach (var error in createResult.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+            return View();
         }
     }
 }
